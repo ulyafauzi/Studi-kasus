85dc61072b22e5952d23be25536dcd95292bae1f
Adnan-kurniawan <102232836+Adnan-kurniawan@users.noreply.github.com>
2022-03-24 09:28:59 +0700

    Update ayamgoyeng.cpp
    Ulya Iqbal fauzi              - 2100018477
    Geraldi Jari Assaji            - 2100018487
    Adnan Arkan Kurniawan - 2100018488

--- ayamgoyeng.cpp
+++ ayamgoyeng.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
-#include <fstream>
-using namespace std;
+#include <iostream>																
+#include <fstream>																
+using namespace std;																
 
 int main (){
 	ofstream print;
